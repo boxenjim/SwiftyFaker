e60ddad661a657c9ce6f3ddddbbe973a8e85b074
Jim Schultz <boxenjim@gmail.com>
2015-09-18 16:14:17 -0600

    added framework to workspace

---
+++ Source/SwiftyFaker.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyFaker.h
+//  SwiftyFaker
+//
+//  Created by Jim Schultz on 9/18/15.
+//  Copyright © 2015 Jim Schultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyFaker.
+FOUNDATION_EXPORT double SwiftyFakerVersionNumber;
+
+//! Project version string for SwiftyFaker.
+FOUNDATION_EXPORT const unsigned char SwiftyFakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyFaker/PublicHeader.h>
+
+
